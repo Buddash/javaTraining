1d6756fdf5043ad39fb6bd1aee34480e54f884c6
Shmelkov Ilia <smelkovila@gmail.com>
2026-05-10 20:57:18 +0400

    Добавлено две программы... trainingLearn1.java производит расчет принимая на вход два аргумента через вод в консоль. trainingLearn2.java описана через метод с двумя аргументами вызываемый в main с параметрами аргументов.

---
+++ HelloJava/src/main/java/org/example/trainingLearn1.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class trainingLearn1 {
+
+    public static void main(String[] args) {
+
+        Scanner inputAge = new Scanner(System.in);
+        System.out.println("Введите возраст: ");
+        int age = inputAge.nextInt();
+
+        Scanner inputSalary = new Scanner(System.in);
+        System.out.println("Введите зарплату: ");
+        int salary = inputSalary.nextInt();
+
+        if ((age >= 18 && age <= 65) || (salary > 35000)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+
+    }
+
+}
